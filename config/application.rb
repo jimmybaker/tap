fc48e04f4c35118137dd58f44d2c09206f637257
Jimmy Baker <jimmyebaker@gmail.com>
2020-04-24 13:29:56 -0500

    wip

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module Tap
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.eager_load_paths << 'app/api'
-    config.eager_load_paths << 'lib'
+    config.autoload_paths += %W[#{config.root}/app/api]
+    config.autoload_paths += %W[#{config.root}/lib]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
