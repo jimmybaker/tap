1bc79889af5bc37fd6a86549d78614849899ed5b
Jimmy Baker <jimmyebaker@gmail.com>
2020-04-24 12:57:12 -0500

    Added task 3 work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +14,29 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+class Person {
+  talk() {
+    return fetch("/api/v1/projects").then((r) => r.json());
+  }
+}
+
+const people = [
+  new Person(),
+  new Person(),
+  new Person(),
+  new Person(),
+  new Person(),
+  new Person(),
+  new Person(),
+  new Person(),
+  new Person(),
+  new Person(),
+];
+
+function converse(callback) {
+  const promises = people.map((p) => p.talk());
+  Promise.all(promises).then(() => callback());
+}
+
+converse(() => alert("done talking"));
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
-  post "/graphql", to: "graphql#execute"
+  post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'task#index'
 
   mount Tap::Base => '/'
 end
